a7416ace53ff6b27aca86e153c0c2baaa355bbc0
Brannen Petit <bpetit940@gmail.com>
2020-05-29 13:01:06 -0500

    finished journal get and post endpoints

--- src/journal/journal-router.js
+++ src/journal/journal-router.js
@@ -7,38 +7,42 @@ const journalRouter = express.Router();
 const jsonBodyParser = express.json();
 
 journalRouter
-  .route('/')
-  .get((req,res,next)=>{
-      JournalService.getAllJournals(req.app.get('db'))
-      .then(journals=>{
-          res.json(journals.map(JournalService.serializeJournal))
-      })
-      .catch(next)
+  .route("/")
+  .get(requireAuth, (req, res, next) => {
+    try {
+      JournalService.getAllJournals(req.app.get("db"), req.user.id).then(
+        (journals) => {
+          res.json(journals.map(JournalService.serializeJournal));
+        }
+      );
+    } catch (error) {
+      next(error);
+    }
   })
-  .post(jsonBodyParser, (req,res,next)=> {
-      const {entry,tasks,emotion}=req.body;
-      JournalService.insert(
-          req.app.get('db'),
-          {entry,tasks,emotion}
-      )
-      .then(journal => {
-          res.status(201).json(journal)
-      })
-  })
-  journalRouter
-    .route('/:journalId')
-    .all(requireAuth)
-    .get((req,res)=>{
-        res.json(JournalService.serializeJournal(res.journal))
+  .post(requireAuth, jsonBodyParser, (req, res, next) => {
+    const user_id = req.user.id;
+    const { entry, tasks, emotion } = req.body;
+    JournalService.insert(req.app.get("db"), user_id, {
+      user_id,
+      entry,
+      tasks,
+      emotion,
+    }).then((journal) => {
+      res.status(201).json(journal);
+    });
+  });
+journalRouter
+  .route("/:journalId")
+  .all(requireAuth)
+  .get((req, res) => {
+    res.json(JournalService.serializeJournal(res.journal));
+  });
+journalRouter.route("/:journalId").delete((req, res, next) => {
+  JournalService.delete(req.app.get("db"), req.params.journalId)
+    .then(() => {
+      res.status(204).end();
     })
-  journalRouter
-    .route('/:journalId')
-    .delete((req,res,next) => {
-        JournalService.delete(req.app.get('db'), req.params.journalId)
-          .then(()=>{
-              res.status(204).end();
-          })
-          .catch(next);
-    })
+    .catch(next);
+});
 
-module.exports=journalRouter;
+module.exports = journalRouter;
