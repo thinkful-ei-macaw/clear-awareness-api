91f15a5a0c70da1a59b717c812d513f826b55709
James Souksavanh Moua <Smoua1558@gmail.com>
2020-05-29 12:01:36 -0400

    create protected-endpoint test

--- test/test-helpers.js
+++ test/test-helpers.js
@@ -2,6 +2,10 @@ const knex = require("knex");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
+/**
+ * create a knex instance connected to postgres
+ * @returns {knex instance}
+ */
 function makeKnexInstance() {
   return knex({
     client: "pg",
@@ -9,6 +13,10 @@ function makeKnexInstance() {
   });
 }
 
+/**
+ * create a knex instance connected to postgres
+ * @returns {array} of user objects
+ */
 function makeUsersArray() {
   return [
     {
@@ -26,6 +34,13 @@ function makeUsersArray() {
   ];
 }
 
+/**
+ * make a bearer token with jwt for authorization header
+ * @param {object} user - contains `id`, `username`
+ * @param {string} secret - used to create the JWT
+ * @returns {string} - for HTTP authorization header
+ */
+
 function makeAuthHeader(user, secret = process.env.JWT_SECRET) {
   const token = jwt.sign({ user_id: user.id }, secret, {
     subject: user.username,
@@ -33,6 +48,12 @@ function makeAuthHeader(user, secret = process.env.JWT_SECRET) {
   });
   return `Bearer ${token}`;
 }
+
+/**
+ * remove data from tables and reset sequences for SERIAL id fields
+ * @param {knex instance} db
+ * @returns {Promise} - when tables are cleared
+ */
 
 function cleanTables(db) {
   return db.transaction((trx) =>
@@ -52,6 +73,13 @@ function cleanTables(db) {
   );
 }
 
+/**
+ * insert users into db with bcrypted passwords and update sequence
+ * @param {knex instance} db
+ * @param {array} users - array of user objects for insertion
+ * @returns {Promise} - when users table seeded
+ */
+
 function seedUsers(db, users) {
   const preppedUsers = users.map((user) => ({
     ...user,
@@ -65,6 +93,8 @@ function seedUsers(db, users) {
     ]);
   });
 }
+
+
 
 module.exports = {
   makeKnexInstance,
