3fd577fee06922e46b612a12b5ba213f8c6bbe7f
Brannen Petit <bpetit940@gmail.com>
2020-06-02 12:52:56 -0500

    sleep endpoint

--- migrations/002.do.create_journal.sql
+++ migrations/002.do.create_journal.sql
@@ -3,6 +3,7 @@ CREATE TABLE "journal" (
     "entry" text NOT NULL,
     "tasks" text NOT NULL,
     "mindful" text NOT NULL,
+    "sleep_hours" INTEGER, 
     "emotions" NUMERIC,
     "user_id" INTEGER REFERENCES "user" (id) ON DELETE CASCADE NOT NULL,
     "date_created" text NOT NULL
--- migrations/002.do.create_sleep.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE "sleep"(
-   "id" SERIAL PRIMARY KEY,
-   "hours" NUMERIC,
-   "user_id" INTEGER REFERENCES "user" (id) ON DELETE CASCADE NOT NULL,
-   "date_created" TIMESTAMPTZ DEFAULT Now()
-);
