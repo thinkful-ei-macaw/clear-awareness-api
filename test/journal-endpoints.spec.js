525722d85499f2e0c4a3cbdf6525bcd6693e3dc6
Desmond Wareham <desmond.wareham@gmail.com>
2020-06-04 16:42:56 -0400

    all endpoints testing

--- test/journal-endpoints.spec.js
+++ test/journal-endpoints.spec.js
@@ -3,7 +3,7 @@ const app = require("../src/app");
 const helpers = require("./test-helpers");
 
 // describe('Journal Endpoints')
-describe.only('Journal Endpoints', function(){
+describe('Journal Endpoints', function(){
     let db
     const testUsers=helpers.makeUsersArray()
     const testUser= testUsers[0];
@@ -30,7 +30,6 @@ describe.only('Journal Endpoints', function(){
     );
     })
   describe(`GET /api/journal`, ()=>{
-      console.log((testUsers[1]))
       it(`responds with 200`, ()=>{
           return supertest(app)
           .get('/api/journal')
@@ -49,38 +48,20 @@ describe.only('Journal Endpoints', function(){
        
     })
   })
-  describe(`PATCH /api/journal/:journalDate`,()=>{
-    it(`should update journal when given valid data and id`, function(){
-        const patchJournal={
-            entry:'new test entry',
-            tasks: 'new test task',
-            mindful: 'new test mindful act',
-            emotion: 3,
-            sleep_hours: 10
-          };
-          return supertest(app)
-            .patch(`/api/journal/:journalDate`)
-            .set("Authorization",helpers.makeAuthHeader(testUsers[1]))
-            .send(patchJournal)
-            .expect(200);
-
-    })
-   
-  })
-
   describe(`POST /api/journal`, ()=>{
     // const {testJournal:testJournals}=helpers.makeJournalFixture()
 
       it(`creates a journal, responding with 201 and the new project`, function(){
         //   this.retries(3)
          let testJournal=testJournals[0]
-          const testUser=testUsers[0]
+          let testUser=testUsers[0]
           const newJournal={
             entry:'new test entry',
             tasks: ['new test task'],
             mindful: 'new test mindful act',
-            emotion: 3,
-            sleep_hours: 10
+            emotions: 3,
+            sleep_hours: 10,
+            date_created:"07/17/1996"
           }
           return supertest(app)
             .post(`/api/journal`)
@@ -93,4 +74,25 @@ describe.only('Journal Endpoints', function(){
       
   })
   
+  describe(`PATCH /api/journal/:journalDate`,()=>{
+    it(`should update journal when given valid data and id`, function(){
+        const patchJournal={
+            id:1,
+            entry:'new test entry',
+            tasks: ['new test task'],
+            mindful: 'new test mindful act',
+            emotions: 3,
+            sleep_hours: 10
+          };
+          return supertest(app)
+            .patch(`/api/journal/${testJournals[0].id}`)
+            .set("Authorization",helpers.makeAuthHeader(testUsers[1]))
+            .send(patchJournal)
+            .expect(204);
+
+    })
+   
+  })
+
+ 
 })
